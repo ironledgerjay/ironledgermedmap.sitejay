6d6c4264dfee34072230903601ae64d72af81f04
mashauofentse420 <mashauofentse420@gmail.com>
2025-08-14 16:29:48 +0200

    Fix homepage to show full website

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from './pages/Index';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Welcome to IronledgerMedMap</h1>} />
+        <Route path="/" element={<Index />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
