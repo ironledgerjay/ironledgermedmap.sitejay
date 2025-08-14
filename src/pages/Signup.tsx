88a1f4802cda57f4ed067b6920c770f6267a870c
mashauofentse420 <mashauofentse420@gmail.com>
2025-08-14 16:23:34 +0200

    Fix supabaseClient import typo

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../superbaseClient';
 
 export default function Signup() {
   const [email, setEmail] = useState('');
